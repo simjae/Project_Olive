59d147f0217f2cb855808b91a43f16e6a279612a
james <blackjack21.chan@gmail.com>
2020-12-31 11:44:09 +0900

    폭풍 전

--- src/main/java/kr/or/hr/controller/HomeController.java
+++ src/main/java/kr/or/hr/controller/HomeController.java
@@ -1,15 +1,19 @@
 package kr.or.hr.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
+import java.io.UnsupportedEncodingException;
 import java.util.Locale;
 
+import org.json.simple.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import kr.or.hr.utils.NewsAPI;
+
 
 @Controller
 public class HomeController {
@@ -18,9 +22,18 @@ public class HomeController {
 	
 
 	@RequestMapping(value = "/Login.do", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		
+	public String home(Locale locale, Model model) {	
 		return "index";
+	}
+	
+	@RequestMapping(value = "/getNewsData.do", method = RequestMethod.GET)
+	@ResponseBody
+	public JSONObject getNewsData() throws UnsupportedEncodingException {
+		NewsAPI newsAPI = new NewsAPI();
+		JSONObject result = newsAPI.returnNewsData();
+		System.out.println(result);
+		return result;
+		
 	}
 	
 }
