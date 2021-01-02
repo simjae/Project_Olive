a9fea289e3ed1e1957abbaa5b4dc1dda29a7d934
HeeSeungPaek <tivkfzlzl@gmail.com>
2021-01-02 18:58:02 +0900

    Check: 선우형한테 테스트

---
+++ src/main/java/com/olive/dto/Dept.java
@@ -0,0 +1,16 @@
+/*
+	파일명: Dept.java
+    설명: 부서 테이블 DTO 객체
+    작성일: 2021-01-02
+    작성자: 백희승
+*/
+package com.olive.dto;
+
+import lombok.Data;
+
+@Data
+public class Dept {
+	private int deptCode;
+	private String deptName;
+	private int headCode;
+}
