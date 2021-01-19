708b84e9a80fd980d169b6a792e62d4a83efda57
Sunwoo <psunu0306@gmail.com>
2021-01-19 11:13:54 +0900

    AOP 편집

--- src/main/java/com/olive/utils/MonitoringAspect.java
+++ src/main/java/com/olive/utils/MonitoringAspect.java
@@ -17,7 +17,7 @@ public class MonitoringAspect {
 	
 
 	//within(com.olive.*.*.*) 나중에 넣어야함 인터페이스로 criteria 빼고 나서 
-	@Around("within(com.olive.*.*.*)")
+	@Around("within(com.olive.hr_info.*.*)")
 	public Object doDAOMonitoring(ProceedingJoinPoint joinpoint) throws Throwable {
 		StopWatch clock = new StopWatch("Profiling ...");
 
